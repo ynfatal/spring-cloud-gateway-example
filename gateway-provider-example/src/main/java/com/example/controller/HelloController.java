eb813e976dc7e68634dea0eb3c904f228dcaefa4
ynFatal <63413763@qq.com>
2020-07-03 06:12:32 +0800

    init spring-cloud-gateway-example

---
+++ gateway-provider-example/src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Fatal
+ * @date 2020/7/2 0002 23:00
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello, Fatal!";
+    }
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
+
+}
