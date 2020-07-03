192d40e64d6503895e86f3a517d1a853e2a51c62
ynFatal <63413763@qq.com>
2020-07-03 22:15:24 +0800

    gateway-include-example remove eureka client.

--- gateway-include-example/src/main/java/com/example/GatewayIncludeExampleApplication.java
+++ gateway-include-example/src/main/java/com/example/GatewayIncludeExampleApplication.java
@@ -2,10 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
 public class GatewayIncludeExampleApplication {
 
     public static void main(String[] args) {
