298e3b7a89a053112ab3df1059720e8457f6e0e4
ynFatal <63413763@qq.com>
2020-07-04 06:28:40 +0800

    init gateway-example4(to be tested)

---
+++ gateway-example4/src/main/java/com/example/GatewayExample4Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayExample4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayExample4Application.class, args);
+    }
+
+}
