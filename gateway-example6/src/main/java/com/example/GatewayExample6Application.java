8d93b06f3d5cbbd44e26d413f50166338cd94887
ynFatal <63413763@qq.com>
2020-07-05 22:11:25 +0800

    init gateway-example6.

---
+++ gateway-example6/src/main/java/com/example/GatewayExample6Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayExample6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayExample6Application.class, args);
+    }
+
+}
