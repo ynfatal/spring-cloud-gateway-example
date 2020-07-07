c9131018bd80fdd344f054fcb1f5062c3c7fab0d
ynFatal <634137063@qq.com>
2020-07-07 18:09:55 +0800

    add fallback for hystrix_route and test it.

---
+++ gateway-example6/src/main/java/com/example/FallbackController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Fatal
+ * @date 2020/7/7 13:03
+ */
+@RestController
+public class FallbackController {
+
+    @GetMapping("/incaseoffailureusethis")
+    public String inCaseOfFailureUseThis() {
+        return "fallback";
+    }
+
+}
