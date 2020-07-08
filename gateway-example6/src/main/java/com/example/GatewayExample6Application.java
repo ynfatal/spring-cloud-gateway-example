6731390114cbed24e96940783c55db8af2e372d5
ynFatal <634137063@qq.com>
2020-07-08 09:31:35 +0800

    1. add PreserveHostHeader GatewayFilter. 2. add RequestRateLimiter GatewayFilter.(to be tested)

--- gateway-example6/src/main/java/com/example/GatewayExample6Application.java
+++ gateway-example6/src/main/java/com/example/GatewayExample6Application.java
@@ -2,12 +2,20 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class GatewayExample6Application {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayExample6Application.class, args);
+    }
+
+    @Bean
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("user"));
     }
 
 }
